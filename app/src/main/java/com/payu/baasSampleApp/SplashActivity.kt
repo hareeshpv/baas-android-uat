d068a8d78ad250ee4f8d558267ed0c08f9d894b0
Tushar Gabani <Tushar.Gabhan@ustechsolutions.com>
2022-06-03 15:05:43 +0530

    1.1(1)

--- app/src/main/java/com/payu/baasSampleApp/SplashActivity.kt
+++ app/src/main/java/com/payu/baasSampleApp/SplashActivity.kt
@@ -14,6 +14,7 @@ open class SplashActivity : BaseActivity() {
         activity = this
         setupNextScreen()
     }
+
     private fun setupNextScreen() {
 
         //From here we launch our UI SDK
